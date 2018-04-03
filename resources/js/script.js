6684f055fe4cfdc715ec9467e2428c1cda091156
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2018-04-03 23:52:16 +0530

    profile improvement

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,46 @@
+const accessDOM = () => {
 
-let profile
-(()=>{
-	
+    return {
+        profilebar: document.getElementsByClassName(`profile-sidebar`)[0],
+        sidebar: document.getElementsByClassName(`sidebar`)[0],
+        smallProfilePic: document.getElementsByClassName(`profile-pic`)[0],
+        largeProfilePic: document.getElementsByClassName(`profile-pic`)[1],
+        back: document.getElementsByClassName(`back`)[0],
+        name: document.querySelector(`.edit input[name='username']`),
+        status: document.querySelector(`.edit input[name='status']`)
+    }
+};
+
+const profilebarVisibility = event => {
+
+    accessDOM().profilebar.classList.remove(`d-none`);
+    accessDOM().sidebar.classList.add(`d-none`);
+};
+
+const sidebarVisibility = event => {
+
+    accessDOM().profilebar.classList.add(`d-none`);
+    accessDOM().sidebar.classList.remove(`d-none`);
+};
+
+const initialUIEvents = () => {
+
+    accessDOM().smallProfilePic.onclick = profilebarVisibility;
+    accessDOM().back.onclick = sidebarVisibility;
+};
+
+const initialUserProfile = () => {
+    accessDOM().smallProfilePic.src = whatsappData.user.profilePicUrl;
+    accessDOM().largeProfilePic.src = whatsappData.user.profilePicUrl;
+    accessDOM().name.value = whatsappData.user.name;
+    accessDOM().status.value = whatsappData.user.status;
+};
+
+const initialUserData = () => {
+    initialUserProfile();
+};
+
+(() => {
+    initialUIEvents();
+    initialUserData();
 })();
