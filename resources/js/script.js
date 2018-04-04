e5342bee202d07df5d6f2373763222dde54c460e
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2018-04-04 21:25:10 +0530

    chats section

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,10 +4,7 @@ const accessDOM = () => {
         profilebar: document.getElementsByClassName(`profile-sidebar`)[0],
         sidebar: document.getElementsByClassName(`sidebar`)[0],
         smallProfilePic: document.getElementsByClassName(`profile-pic`)[0],
-        largeProfilePic: document.getElementsByClassName(`profile-pic`)[1],
-        back: document.getElementsByClassName(`back`)[0],
-        name: document.querySelector(`.edit input[name='username']`),
-        status: document.querySelector(`.edit input[name='status']`)
+        back: document.getElementsByClassName(`back`)[0]
     }
 };
 
@@ -29,18 +26,7 @@ const initialUIEvents = () => {
     accessDOM().back.onclick = sidebarVisibility;
 };
 
-const initialUserProfile = () => {
-    accessDOM().smallProfilePic.src = whatsappData.user.profilePicUrl;
-    accessDOM().largeProfilePic.src = whatsappData.user.profilePicUrl;
-    accessDOM().name.value = whatsappData.user.name;
-    accessDOM().status.value = whatsappData.user.status;
-};
-
-const initialUserData = () => {
-    initialUserProfile();
-};
-
 (() => {
+
     initialUIEvents();
-    initialUserData();
 })();
