9d32fd397531d11a352bf4f89d9d8d45332c275f
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2018-04-06 23:44:37 +0530

    click events

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,7 +4,11 @@ const accessDOM = () => {
         profilebar: document.getElementsByClassName(`profile-sidebar`)[0],
         sidebar: document.getElementsByClassName(`sidebar`)[0],
         smallProfilePic: document.getElementsByClassName(`profile-pic`)[0],
-        back: document.getElementsByClassName(`back`)[0]
+        largeProfilePic: document.getElementsByClassName(`profile-pic`)[1],
+        back: document.getElementsByClassName(`back`)[0],
+        name: document.querySelector(`[name='displayName']`),
+        about: document.querySelector(`[name='about']`),
+        chats: document.querySelectorAll(`.chats li`)
     }
 };
 
@@ -20,13 +24,40 @@ const sidebarVisibility = event => {
     accessDOM().sidebar.classList.remove(`d-none`);
 };
 
-const initialUIEvents = () => {
+const userClicksChat = event => {
+
+    const targetLi = event.path.find(x => x.localName === 'li');
+
+    for (let user of whatsappData.communication[targetLi.getAttribute('data-type')]) {
+        if (user.name === targetLi.getAttribute('data-name')) {
+            
+            
+            break;
+        }
+    }
+    event.stopPropagation();
+};
+
+const initializeUIEvents = () => {
 
     accessDOM().smallProfilePic.onclick = profilebarVisibility;
     accessDOM().back.onclick = sidebarVisibility;
+
+    for (let chat in accessDOM().chats) {
+        accessDOM().chats[chat].onclick = userClicksChat;
+    }
+};
+
+
+const initializeProfileData = () => {
+
+    accessDOM().largeProfilePic.src = whatsappData.userData.profilePicUrl;
+    accessDOM().name.value = whatsappData.userData.name;
+    accessDOM().about.value = whatsappData.userData.about;
 };
 
 (() => {
 
-    initialUIEvents();
+    initializeUIEvents();
+    initializeProfileData();
 })();
---
+++ resources/js/whatsappdata.js
@@ -0,0 +1,102 @@
+const whatsappData = {
+    "userData": {
+        "name": "Paul McCartney",
+        "about": "Hey Jude!",
+        "profilePicUrl": "./resources/img/profile-pic.jpg"
+    },
+    "communication": {
+        "oneToOne": [{
+            "name": "Janet Jackson",
+            "profilePicUrl": "./resources/img/janet.jpg",
+            "lastSeen": "7:03 PM",
+            "message": [{
+                "date": "3/4/2018",
+                "message": [{
+                        "type": "self",
+                        "text": "Hi Janet<br>Paul here",
+                        "time": "1:40 PM",
+                        "status": true
+                    }, {
+                        "type": "other",
+                        "text": "Hiiii😊😊... how are u???",
+                        "time": "2:35 PM",
+                        "seen": true
+                    }, {
+                        "type": "self",
+                        "text": "I am fine wbuu",
+                        "time": "2:40 PM",
+                        "seen": true
+                    }, {
+                        "type": "self",
+                        "text": "Fine😊😊...are u working??",
+                        "time": "2:45 PM",
+                        "seen": true
+                    }, {
+                        "type": "self",
+                        "text": "Studyying online",
+                        "time": "2:47 PM",
+                        "seen": true
+                    }, {
+                        "type": "self",
+                        "text": "Are you working??",
+                        "time": "2:50 PM",
+                        "seen": true
+                    },
+                    {
+                        "type": "other",
+                        "text": "Yup... what are u studying",
+                        "time": "2:52 PM",
+                        "seen": true
+                    }, {
+                        "type": "other",
+                        "text": "??",
+                        "time": "2:55 PM",
+                        "seen": true
+                    }, {
+                        "type": "self",
+                        "text": "Web Development",
+                        "time": "2:57 PM",
+                        "seen": true
+                    }, {
+                        "type": "other",
+                        "text": "K👍",
+                        "time": "2:59 PM",
+                        "seen": true
+                    }, {
+                        "type": "self",
+                        "text": "What is your Degree/Major In?",
+                        "time": "3:02 PM",
+                        "seen": true
+                    }, {
+                        "type": "other",
+                        "text": "Information Technology",
+                        "time": "3:02 PM",
+                        "seen": true
+                    }
+                ]
+            }]
+        }, {
+            "name": "Dave Grohl",
+            "profilePicUrl": "./resources/img/dave.jpg",
+            "lastSeen": "Yesterday"
+        },{
+        	"name": "Emily Vulcano",
+            "profilePicUrl": "./resources/img/emily.jpg",
+            "lastSeen": "6:33 PM"
+        },{
+        	"name": "John Gatto",
+            "profilePicUrl": "./resources/img/john.jpg",
+            "lastSeen": "1:01 PM"
+        }],
+        "group": [{
+            "name": "The Avengers",
+            "profilePicUrl": "./resources/img/avengers.jpg",
+            "lastPost": "Yesterday"
+        }, {
+            "name": "LA BOYS",
+            "profilePicUrl": "./resources/img/la.png",
+            "lastPost": "2/3/2018"
+        }]
+    }
+
+}
