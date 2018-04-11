3cc4167ed3b72f2573db8d6afe6cecf0bf10bdfb
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2018-04-11 23:32:59 +0530

    navigation and bug fixed

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,34 +1,61 @@
 const accessDOM = () => {
 
     return {
-        profilebar: document.getElementsByClassName(`profile-sidebar`)[0],
-        sidebar: document.getElementsByClassName(`sidebar`)[0],
-        smallProfilePic: document.getElementsByClassName(`profile-pic`)[0],
-        largeProfilePic: document.getElementsByClassName(`profile-pic`)[1],
-        back: document.getElementsByClassName(`back`)[0],
-        displayName: document.querySelector(`[name='displayName']`),
-        about: document.querySelector(`[name='about']`),
-        chats: document.querySelectorAll(`.chats li`),
-        chatWindowName: document.querySelector(`.chat .chat-window_name`),
-        chatWindowPhoto: document.querySelector(`.chat img`),
-        details: document.querySelector(`.chat .details`),
+        profilebar: document.getElementsByClassName('profile-sidebar')[0],
+        sidebar: document.getElementsByClassName('sidebar')[0],
+        smallProfilePic: document.getElementsByClassName('profile-pic')[0],
+        largeProfilePic: document.getElementsByClassName('profile-pic')[1],
+        backFromProfile: document.querySelector('.profile-sidebar .back'),
+        backFromChat: document.querySelector('.chat .back'),
+        displayName: document.querySelector('[name="displayName"]'),
+        about: document.querySelector('[name="about"]'),
+        chats: document.querySelectorAll('.chats li'),
+        chatWindowName: document.querySelector('.chat .chat-window_name'),
+        chatWindowPhoto: document.querySelector('.chat img'),
+        details: document.querySelector('.chat .details'),
         clickedList(event) { return event.path.find(x => x.localName === 'li') },
-        chatSection: document.querySelector(`.chat-section`),
-        unreadMessage: document.querySelector(`.chats .col-3 div`)
+        chatSection: document.querySelector('.chat-section'),
+        unreadMessage: document.querySelector('.chats .col-3 div'),
+        chat: document.querySelector('.chat')
     }
 };
 
 const profilebarVisibility = event => {
 
-    accessDOM().profilebar.classList.remove(`d-none`);
-    accessDOM().sidebar.classList.add(`d-none`);
+    if (accessDOM().sidebar.classList.contains('d-block')) {
+        accessDOM().sidebar.classList.remove('d-block');
+        accessDOM().sidebar.classList.add('d-none');
+    }
+
+    accessDOM().profilebar.classList.remove('d-none');
+    accessDOM().profilebar.classList.add('d-block');
 };
 
-const sidebarVisibility = event => {
+const sidebarVisibilityFromProfile = event => {
 
-    accessDOM().profilebar.classList.add(`d-none`);
-    accessDOM().sidebar.classList.remove(`d-none`);
+
+    accessDOM().profilebar.classList.add('d-none');
+    accessDOM().profilebar.classList.remove('d-block');
+    accessDOM().sidebar.classList.remove('d-none');
+    accessDOM().sidebar.classList.add('d-block');
 };
+
+const sidebarVisibilityFromChat = event => {
+    if (window.innerWidth < 768) {
+        accessDOM().chat.classList.add('d-none');
+        accessDOM().chat.classList.remove('d-block');
+        accessDOM().sidebar.classList.remove('d-none');
+        accessDOM().sidebar.classList.add('d-block');
+    }
+};
+
+const hideNewMessageIndicator = event => {
+
+    if (accessDOM().clickedList(event).children[2].children[2]) {
+
+        accessDOM().clickedList(event).children[2].children[2].classList.add('d-none');
+    }
+}
 
 const changeListColor = event => {
 
@@ -75,6 +102,34 @@ const receiverHTML = (index, currentType, previousType, text, time) => {
                 	    <div class = 'text-muted position-absolute'>${time}</div></div></div>`;
 };
 
+const singleDateMessages = singleDate => {
+
+    for (let m in singleDate.message) {
+
+        if (singleDate.message[m].type === 'self') {
+
+            accessDOM().chatSection.insertAdjacentHTML('beforeend',
+                senderHTML(m,
+                    singleDate.message[m].type,
+                    m > 0 ? singleDate.message[m - 1].type : undefined,
+                    singleDate.message[m].text,
+                    singleDate.message[m].time,
+                    singleDate.message[m].seen
+                ));
+
+        } else if (singleDate.message[m].type === 'other') {
+
+            accessDOM().chatSection.insertAdjacentHTML('beforeend',
+                receiverHTML(m,
+                    singleDate.message[m].type,
+                    m > 0 ? singleDate.message[m - 1].type : undefined,
+                    singleDate.message[m].text,
+                    singleDate.message[m].time
+                ));
+        }
+    }
+};
+
 const loadChat = userOrGrp => {
 
     if (userOrGrp.messages) {
@@ -83,30 +138,8 @@ const loadChat = userOrGrp => {
 
         for (let singleDate of userOrGrp.messages) {
 
-            for (let m in singleDate.message) {
+            singleDateMessages(singleDate);
 
-                if (singleDate.message[m].type === 'self') {
-
-                    accessDOM().chatSection.insertAdjacentHTML('beforeend',
-                        senderHTML(m,
-                            singleDate.message[m].type,
-                            m > 0 ? singleDate.message[m - 1].type : undefined,
-                            singleDate.message[m].text,
-                            singleDate.message[m].time,
-                            singleDate.message[m].seen
-                        ));
-
-                } else if (singleDate.message[m].type === 'other') {
-
-                    accessDOM().chatSection.insertAdjacentHTML('beforeend',
-                        receiverHTML(m,
-                            singleDate.message[m].type,
-                            m > 0 ? singleDate.message[m - 1].type : undefined,
-                            singleDate.message[m].text,
-                            singleDate.message[m].time
-                        ));
-                }
-            }
         }
     }
 };
@@ -132,9 +165,16 @@ const initializeChatDOM = userOrGrp => {
 
 const userClicksChat = event => {
 
-    if (accessDOM().clickedList(event).children[2].children[2]) {
+    hideNewMessageIndicator(event);
 
-        accessDOM().clickedList(event).children[2].children[2].classList.add('d-none');
+    if (window.innerWidth < 768) {
+        
+        accessDOM().sidebar.classList.remove('d-block');
+        accessDOM().sidebar.classList.add('d-none');
+
+        accessDOM().chat.classList.remove('d-none');
+        accessDOM().chat.classList.add('d-block');
+
     }
 
     for (let userOrGrp of whatsappData.communication[accessDOM().clickedList(event).getAttribute('data-type')]) {
@@ -155,7 +195,35 @@ const userClicksChat = event => {
 const initializeUIEvents = () => {
 
     accessDOM().smallProfilePic.onclick = profilebarVisibility;
-    accessDOM().back.onclick = sidebarVisibility;
+    accessDOM().backFromProfile.onclick = sidebarVisibilityFromProfile;
+    accessDOM().backFromChat.onclick = sidebarVisibilityFromChat;
+
+    window.addEventListener("resize", () => {
+        
+        if (window.innerWidth >= 768) {
+            if (accessDOM().sidebar.classList.contains('d-none')) {
+                if (accessDOM().profilebar.classList.contains('d-none')) {
+
+
+                    accessDOM().sidebar.classList.add('d-block');
+                    accessDOM().sidebar.classList.remove('d-none');
+                }
+            }
+            
+            accessDOM().chat.classList.add('d-block');
+            accessDOM().chat.classList.remove('d-none');
+
+
+        } else if (window.innerWidth < 768) {
+
+        	if(accessDOM().sidebar.classList.contains('d-block')||accessDOM().profilebar.classList.contains('d-block')){ 
+           accessDOM().chat.classList.add('d-none');
+            accessDOM().chat.classList.remove('d-block');
+        
+        }
+    }
+
+    });
 
     for (let chat in accessDOM().chats) {
 
@@ -176,4 +244,9 @@ const initializeProfileData = () => {
     initializeUIEvents();
     initializeProfileData();
     initializeChatDOM(whatsappData.communication.oneToOne[0]);
+
+    if (window.innerWidth < 768) {
+        accessDOM().chat.classList.add('d-none');
+        accessDOM().chat.classList.remove('d-block');
+    }
 })();
